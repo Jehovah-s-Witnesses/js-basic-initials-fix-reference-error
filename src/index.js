5ea2eaa53b7c81f98718439244b9a2d314cde2ec
gribVolnushka <burgerqueen98@gmail.com>
2024-08-25 13:44:36 +0200

    Changed names of variables and functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 const login = prompt('Enter login');
 const password = prompt('Enter password');
 
-if (login === 'admin') {
-  if (passwrd === 'qwerty1234') {
-    alrt('Hello, admin');
+if (login === 'admin' && password === 'qwerty1234') {
+  alert ('Hello, admim');
+} else if (login === 'guest' && password === 'guest666') {
+    alert('Hello, guest');
+  } else if (login !== 'admin' && login !== 'guest') {
+   alert('Go away.');
+  } else {
+    alert('Incorrect info.');
+  }
+
+
+
+
+/*if (login === 'admin') {
+  if (password === 'qwerty1234') {
+    alert('Hello, admin');
   } else {
     alert('Incorrect info');
   }
-} else if (logn === 'Guest') {
+} else if (login === 'Guest') {
   if (password === 'guest666') {
     alert('Hello, guest');
   } else {
-    alrt('Incorrect info');
+    alert('Incorrect info');
   }
 } else {
   alert('Go away');
 }
+  */
