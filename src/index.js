fc6b4537f5dc9e914ece90c9882c2ede97df2f0e
gribVolnushka <burgerqueen98@gmail.com>
2024-08-27 12:09:56 +0200

    fixed all errors

--- src/index.js
+++ src/index.js
@@ -2,31 +2,11 @@ const login = prompt('Enter login');
 const password = prompt('Enter password');
 
 if (login === 'admin' && password === 'qwerty1234') {
-  alert ('Hello, admim');
+  alert('Hello, admin');
 } else if (login === 'guest' && password === 'guest666') {
-    alert('Hello, guest');
-  } else if (login !== 'admin' && login !== 'guest') {
-   alert('Go away.');
-  } else {
-    alert('Incorrect info.');
-  }
-
-
-
-
-/*if (login === 'admin') {
-  if (password === 'qwerty1234') {
-    alert('Hello, admin');
-  } else {
-    alert('Incorrect info');
-  }
-} else if (login === 'Guest') {
-  if (password === 'guest666') {
-    alert('Hello, guest');
-  } else {
-    alert('Incorrect info');
-  }
+  alert('Hello, guest');
+} else if (login !== 'admin' && login !== 'guest') {
+  alert('Go away.');
 } else {
-  alert('Go away');
+  alert('Incorrect info.');
 }
-  */
